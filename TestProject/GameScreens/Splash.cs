29b661971f9039ec07ca695d002bb8dac71f0614
Saiboot <t3eg3e@gmail.com>
2017-02-06 00:53:52 +0100

    Buuuuu

--- TestProject/GameScreens/Splash.cs
+++ TestProject/GameScreens/Splash.cs
@@ -34,7 +34,6 @@ namespace TestProject.GameScreens
                 Core.currentScreen = ScreenStates.Menu;
             }
             #endregion
-
         }
 
         public override void Draw(SpriteBatch spriteBatch)
