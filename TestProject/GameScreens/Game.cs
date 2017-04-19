49dcfdc8e751c88bfffc32e74951a586fc3bb693
Tobias <T3eG3e@gmail.com>
2017-04-19 14:33:06 +0200

    Double call prevented.

--- TestProject/GameScreens/Game.cs
+++ TestProject/GameScreens/Game.cs
@@ -25,8 +25,7 @@ namespace TestProject.GameScreens
         }
         public override void LoadContent(ContentManager Content)
         {
-            Character = new Entities.Player(Content.Load<Texture2D>("pump"), new Rectangle(Point.Zero, new Point(32, 32)), true);
-            Character.Texture = Content.Load<Texture2D>("Fighter");
+            Character = new Entities.Player(Content.Load<Texture2D>("Fighter"), new Rectangle(Point.Zero, new Point(32, 32)), true);
             font = Content.Load<SpriteFont>("Code");
 
             Particle = new Entities.Particle(Content.Load<Texture2D>("Particle"), new Rectangle(Point.Zero, new Point(32, 32)), true, Vector2.Zero);
